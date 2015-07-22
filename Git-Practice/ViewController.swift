147a8df73ccca4f0ff0d639d187a547cf61f8dde
Emma-Ade <adeniyibte@gmail.com>
2015-07-22 12:34:14 +0100

    update from server

--- Git-Practice/ViewController.swift
+++ Git-Practice/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        println("Edit from server")
         println(" 8f4d61feb51a82a9e7c8925199fd6c2817202a25")
     }
 
